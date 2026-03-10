d5003e3ad2443e9fd15e3564a9db12a4c7e20baa
mingyu012605 <songmingyu922@gmail.com>
2026-03-10 14:38:53 -0700

    Update OpenEphysLib.cpp

--- Source/OpenEphysLib.cpp
+++ Source/OpenEphysLib.cpp
@@ -1,5 +1,6 @@
 #include <PluginInfo.h>
-#include "TTLEventGenerator.h"
+#include "TTLEventGenerator.h" // <---- 이 부분을 "VisualizerPlugin.h"에서 바꿨어!
+
 #include <string>
 
 #ifdef WIN32
@@ -15,8 +16,11 @@ using namespace Plugin;
 
 extern "C" EXPORT void getLibInfo(Plugin::LibraryInfo* info)
 {
+   /* API version, defined by the GUI source.
+   Should not be changed to ensure it is always equal to the one used in the latest codebase.
+   The GUI refuses to load plugins with mismatched API versions */
    info->apiVersion = PLUGIN_API_VER;
-   info->name = "TTL Event Generator"; 
+   info->name = "TTL Event Generator"; // 가이드대로 업데이트
    info->libVersion = "0.1.0"; 
    info->numPlugins = NUM_PLUGINS;
 }
@@ -27,9 +31,9 @@ extern "C" EXPORT int getPluginInfo(int index, Plugin::PluginInfo* info)
    {
    case 0:
       info->type = Plugin::Type::PROCESSOR;
-      info->processor.name = "TTL Event Generator"; 
-      info->processor.type = Processor::Type::FILTER;
-      info->processor.creator = &(Plugin::createProcessor<TTLEventGenerator>); 
+      info->processor.name = "TTL Event Generator"; // 가이드대로 업데이트
+      info->processor.type = Processor::Type::FILTER; // 가이드대로 업데이트
+      info->processor.creator = &(Plugin::createProcessor<TTLEventGenerator>); // 가이드대로 업데이트
       break;
    default:
       return -1;
