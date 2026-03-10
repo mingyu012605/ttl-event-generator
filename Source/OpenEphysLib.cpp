1802ddc537c16f237b7745d99475b4db38fd83ea
mingyu012605 <songmingyu922@gmail.com>
2026-03-10 14:34:36 -0700

    Update VisualizerPlugin to TTL Event Generator

--- Source/OpenEphysLib.cpp
+++ Source/OpenEphysLib.cpp
@@ -1,29 +1,5 @@
-/*
-    ------------------------------------------------------------------
-
-    This file is part of the Open Ephys GUI
-    Copyright (C) 2025 Open Ephys
-
-    ------------------------------------------------------------------
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include <PluginInfo.h>
-
-#include "VisualizerPlugin.h" // update to point to your custom header file
-
+#include "TTLEventGenerator.h"
 #include <string>
 
 #ifdef WIN32
@@ -35,52 +11,36 @@
 
 using namespace Plugin;
 
-#define NUM_PLUGINS 1 // usually 1, but can be more if there are multiple plugins per repository
+#define NUM_PLUGINS 1 
 
-extern "C" EXPORT void getLibInfo (Plugin::LibraryInfo* info)
+extern "C" EXPORT void getLibInfo(Plugin::LibraryInfo* info)
 {
-    /* API version, defined by the GUI source.
-	Should not be changed to ensure it is always equal to the one used in the latest codebase.
-	The GUI refueses to load plugins with mismatched API versions */
-    info->apiVersion = PLUGIN_API_VER;
-    info->name = "VisualizerPlugin"; // Name of the plugin library
-    info->libVersion = "0.1.0"; //Version of the plugin
-    info->numPlugins = NUM_PLUGINS;
+   info->apiVersion = PLUGIN_API_VER;
+   info->name = "TTL Event Generator"; 
+   info->libVersion = "0.1.0"; 
+   info->numPlugins = NUM_PLUGINS;
 }
 
-extern "C" EXPORT int getPluginInfo (int index, Plugin::PluginInfo* info)
+extern "C" EXPORT int getPluginInfo(int index, Plugin::PluginInfo* info)
 {
-    switch (index)
-    {
-            //one case per plugin. This example is for a processor which connects directly to the signal chain
-        case 0:
-
-            //Type of plugin. See "Source/Processors/PluginManager/OpenEphysPlugin.h" for complete info about the different type structures
-            info->type = Plugin::Type::PROCESSOR;
-
-            //Processor name
-            info->processor.name = "Visualizer Plugin"; // Processor name shown in the GUI
-
-            //Type of processor. Visualizers are usually sinks, but they can also be SOURCE or FILTER processors.
-            info->processor.type = Processor::Type::SINK;
-
-            //Class factory pointer. Replace "ProcessorPluginSpace::ProcessorPlugin" with the namespace and class name.
-            info->processor.creator = &(Plugin::createProcessor<VisualizerPlugin>);
-            break;
-
-        default:
-            return -1;
-            break;
-    }
-    return 0;
+   switch (index)
+   {
+   case 0:
+      info->type = Plugin::Type::PROCESSOR;
+      info->processor.name = "TTL Event Generator"; 
+      info->processor.type = Processor::Type::FILTER;
+      info->processor.creator = &(Plugin::createProcessor<TTLEventGenerator>); 
+      break;
+   default:
+      return -1;
+      break;
+   }
+   return 0;
 }
 
 #ifdef WIN32
-BOOL WINAPI DllMain (IN HINSTANCE hDllHandle,
-                     IN DWORD nReason,
-                     IN LPVOID Reserved)
+BOOL WINAPI DllMain(IN HINSTANCE hDllHandle, IN DWORD nReason, IN LPVOID Reserved)
 {
-    return TRUE;
+   return TRUE;
 }
-
 #endif
